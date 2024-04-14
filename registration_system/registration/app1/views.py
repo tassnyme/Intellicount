68de2369a5d95cd5e250dbae2dc6c639253f8b99
Tassnyme <tassnymelaroussy@gmail.com>
2024-04-14 18:49:43 +0100

    first commit

---
+++ registration_system/registration/app1/views.py
@@ -0,0 +1,172 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect 
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
+
+@login_required(login_url='login')
+def home(request):
+    return render (request,'home.html') 
+
+def user_profile(request):
+    return render (request,'user_profile.html')
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+        
+        # Check if passwords match
+        if pass1 != pass2:
+            return render(request, "signup.html", {'msg': ['Passwords do not match']}) 
+        else:
+            # Create user
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.is_active = False
+            my_user.save()
+            return render(request, "signup.html", {'msg1': ['Account created, please wait for the expert verification']}) 
+        
+    else:
+        # Return a default response if the request method is not POST
+        return render(request, 'signup.html')
+    
+
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if(user.is_superuser):
+                return redirect('account')  # Redirect to the 'accounts' page upon successful login
+            else:
+                return redirect('dashboard_user') 
+        else:
+            return render(request, "login.html", {'msg2': ['Watch Out ! Username or Password incorrect.']}) 
+    
+    return render(request, 'login.html')  # Render the login page template for GET requests
+
+
+
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def account(request):
+    users = User.objects.all()
+    return render(request,'account.html',{'users': users})
+
+def user_dash(request):
+    users = User.objects.all()
+    return render(request, 'dashboard_user.html', {'users': users})
+
+def Deleteuser(request,pk):
+    uname=User.objects.get(pk=pk)
+    b=User.objects.filter(username=uname)
+    b.delete()
+    # Add a success message
+    messages.success(request, 'User account has been successfully deleted.')
+    return redirect('account')
+
+def update(request,pk):
+    user=User.objects.get(pk=pk)
+    return render(request,'update.html',{'user':user})
+
+def updateuser(request, pk):
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        fname = request.POST.get('first_name')
+        lname = request.POST.get('last_name')
+        user = User.objects.get(pk=pk)
+        user.first_name = fname
+        user.last_name = lname
+        user.username = name
+        user.email = email
+        user.save()
+        # Add a success message
+        messages.success(request, 'User account has been successfully updated.')
+        return redirect('account')
+
+# def updatprofile(request, pk):
+#     if request.method == 'POST':
+#         name = request.POST.get('username')
+#         email = request.POST.get('email')
+#         fname = request.POST.get('first_name')
+#         lname = request.POST.get('last_name')
+#         user = User.objects.get(pk=pk)
+#         user.first_name = fname
+#         user.last_name = lname
+#         user.username = name
+#         user.email = email
+#         user.save()
+#         # Add a success message
+#         messages.success(request, 'User account has been successfully updated.')
+#         return redirect('user_profile')
+
+def AddUser(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        fname=request.POST.get('first_name')
+        lname=request.POST.get('last_name')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+        
+        # Check if passwords match
+        if pass1 != pass2:
+            return HttpResponse("Passwords do not match")
+        else :
+            # Create user
+            my_user = User.objects.create_user(uname,email,pass1)
+            my_user.is_active=False
+            my_user.save()
+            # Add a success message
+            messages.success(request, 'User account has been successfully added.')
+            return redirect('account') #use the name from views.py
+    else :
+        # Return a default response if the request method is not POST
+        return render(request,'add_user.html')  
+
+def verify_user(request, pk):
+    # Check if the logged-in user is an admin
+    if request.user.is_superuser:
+        try:
+            # Retrieve the user object by ID
+            user = User.objects.get(pk=pk)
+            if not user.is_active:  # Check if user is not already active
+                # Update user permissions to grant access to log in
+                user.is_active = True
+            
+                # Email sender
+                subject = "Account Verification" 
+                message = f"Dear {user.username}, your account has been verified now. You are now able to access your account."
+                email = user.email
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [email], fail_silently=True) 
+                # Save user
+                user.save()
+                # Add a success message for verification
+                messages.success(request, 'User account has been successfully verified.')
+            else:
+                # Update user permissions to revoke access
+                user.is_active = False
+                # Add a success message for unverification
+                messages.success(request, 'User account has been unverified.')
+            user.save()  # Save user changes
+
+        except User.DoesNotExist:
+            # Handle case where user does not exist
+            messages.error(request, 'User does not exist.')
+    else:
+        # Return unauthorized access message
+        messages.error(request, 'Unauthorized access.')
+
+    return redirect('account')  # Redirect admin to admin dashboard after verification
---
+++ registration_system/registration/registration/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for registration project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app1 import views 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/',views.SignupPage,name='signup'),
+    path('',views.LoginPage,name="login"),
+    path('home/',views.home,name='logout'),
+    path('user_profile/',views.user_profile,name='user_profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('Accounts/', views.account, name='account'),
+    path('dashboard_user/',views.user_dash , name= 'dashboard_user'),
+    path('Delete_users/(?P<pk>\d+)/',views.Deleteuser,name='Delete'),
+    path('update/(?P<pk>\d+)/',views.update,name='update'),
+    path('update_users/(?P<pk>\d+)/', views.updateuser, name='updateuser'),
+    # path('updatprofile/(?P<pk>\d+)/', views.updatprofile, name='updatprofile'),
+    path('add_user/',views.AddUser,name='add_user'),
+    path('verify/(?P<pk>\d+)/',views.verify_user,name='verify'),
+
+]
